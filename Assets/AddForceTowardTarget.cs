1edd1296d3a69511a06da239b8466eac732266b3
Oicirtap <patricionoyola@gmail.com>
2016-04-25 13:54:39 -0400

    NPC basic behavior

---
+++ Assets/AddForceTowardTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForceTowardTarget : MonoBehaviour {
+
+	public Transform target;
+
+	private Rigidbody rb;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void Update () {
+		rb.AddForce (target.position - transform.position);
+	}
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteraction : MonoBehaviour {
+
+	public string keyString;
+
+	private bool isFollowing;
+
+	void Start () {
+		isFollowing = false;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (keyString) && !isFollowing) {
+			Debug.Log ("set to true");
+			isFollowing = true;
+		} else if (Input.GetKeyDown (keyString)) {
+			Debug.Log ("set to false");
+			isFollowing = false;
+		}
+	}
+
+	public bool getIsFollowing () {
+		return isFollowing;
+	}
+}
