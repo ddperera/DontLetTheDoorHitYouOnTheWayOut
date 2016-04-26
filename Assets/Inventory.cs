d22c51da97e131ec75008dd207c81ae44ea9f244
Ari Green <arigreen@mit.edu>
2016-04-26 14:36:21 -0400

    Added Inventory And Money Systems

---
+++ Assets/Inventory.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	// ! Inventory Items must be given unique names !
+	Dictionary<string,GameObject> myItems = new Dictionary<string,GameObject>();
+	int myMoney = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddObject(string name, GameObject item){
+		myItems.Add(name,item);
+	}
+
+	public void RemoveObject(string name){
+		myItems.Remove(name);
+	}
+
+	public bool OwnsObject(string name){
+		return myItems.ContainsKey(name);
+	}
+
+	public bool OwnsObject(GameObject item){
+		return myItems.ContainsValue(item);
+	}
+
+	public void AddMoney(int amount){
+		myMoney += amount;
+	}
+
+	public void SubtractMoney(int amount){
+		myMoney -= amount;
+	}
+
+	public bool HasEnoughMoney(int price){
+		return myMoney >= price;
+	}
+
+	public void PurchaseFor(int price){
+		if (HasEnoughMoney(price)) {
+			SubtractMoney (price);
+		}
+	}
+}
---
+++ Assets/Scripts/DoorHandleBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorHandleBehavior : MonoBehaviour {
+
+	GameObject thePlayer;s
+	string myName = "Level2_DoorHandle_1";
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPlayerClicked(){
+		//play some acquiring sound
+		thePlayer.GetComponent<Inventory>().AddObject(myName, gameObject);
+		gameObject.SetActive (false);
+	}
+}
