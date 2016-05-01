6b36dffcc7dfa1731e684fd01e38942a3b8f597c
nishchalb <nishchal2050@gmail.com>
2016-05-01 00:00:12 -0400

    Implemented sign and falling behavior for level 1

---
+++ Assets/Scripts/SignBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignBehavior : MonoBehaviour {
+
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnPlayerClicked() {
+		rb.isKinematic = false;
+		Debug.Log("sign fall", this);
+	}
+}
