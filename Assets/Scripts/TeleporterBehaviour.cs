5ac90eb21e840f9c8678157dcd7761a54d0a4052
ddperera <ddperera@mit.edu>
2016-05-04 12:26:41 -0400

    added teleporting path selection to level 1

---
+++ Assets/Scripts/TeleporterBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleporterBehaviour : MonoBehaviour {
+
+	public Transform destination;
+	private bool solvedPuzzle = false;
+	private GameObject player;
+	private Movement playerInfo;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerInfo = player.GetComponent<Movement> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.CompareTag ("Player")) {
+			if (playerInfo.isGhost) {
+				solvedPuzzle = true;
+			}
+
+			if (!solvedPuzzle) {
+				player.transform.position = destination.position;
+			}
+		}
+	}
+}
