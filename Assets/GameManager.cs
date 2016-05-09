3b8c635a64c6a5e875a7afaf1ee64d3f50572c4b
Oicirtap <patricionoyola@gmail.com>
2016-05-08 23:14:41 -0400

    added persistend data object to track game progress throughout scenes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gm;
+
+	//[HideInInspector]
+
+	public bool level1Cleared;
+	public bool level2Cleared;
+
+	void Awake () {
+		if (gm == null) {
+			DontDestroyOnLoad (gameObject);
+			gm = this;
+		} else if (gm != this){
+			Destroy (gameObject);
+		}
+	}
+}
