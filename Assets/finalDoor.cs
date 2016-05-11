1a29b260d883bf7d0ac125247c8be3382604932d
Oicirtap <patricionoyola@gmail.com>
2016-05-11 12:33:04 -0400

    fixed final door

---
+++ Assets/finalDoor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finalDoor : MonoBehaviour {
+
+	private GameObject thePlayer;
+	private bool locked = true;
+	private bool needsKey = false;
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnPlayerClicked(){
+		if (locked) {
+			if (GameManager.gm != null && GameManager.gm.level1Cleared && GameManager.gm.level2Cleared) {
+				locked = false;
+				thePlayer.GetComponent<AudioSource> ().PlayOneShot (gameObject.GetComponent<AudioSource>().clip);
+				// Lock visual effect (move lock part up)
+				this.gameObject.tag = "Interactable";
+			}
+		} else {
+
+			this.gameObject.GetComponent<Animator>().SetTrigger("openDoor");
+			//this.gameObject.SetActive (false);
+		}
+
+	}
+}
